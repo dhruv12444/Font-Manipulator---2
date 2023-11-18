b21c60026d8dc8316cfff3154babe1f1592ac679
dhruv12444 <124359650+dhruv12444@users.noreply.github.com>
2023-11-18 18:38:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded) ;
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background('#FF7276');
+}
+
+function modelLoaded()
+{
+    console.log('Posenet is initiallized!');
+}
+
+function gotPoses(results)
+{
+if(results.length > 0)
+{
+    console.log(results)
+}
+}
